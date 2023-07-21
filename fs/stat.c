e0b4ecaf966985aadead5439250727e5917a5b9c
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-07-21 13:20:38 +0700

    Re-check XSU

--- fs/stat.c
+++ fs/stat.c
@@ -87,16 +87,16 @@ int vfs_fstat(unsigned int fd, struct kstat *stat)
 }
 EXPORT_SYMBOL(vfs_fstat);
 
-extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
+extern int ksu_handle_stat(int *dfd, const char __user **filename_user);
 
 int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
 		int flag)
 {
 	struct path path;
 	int error = -EINVAL;
-	unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;
-        ksu_handle_stat(&dfd, &filename, &flags);
-
+	unsigned int lookup_flags = 0;
+	
+        ksu_handle_stat(&dfd, &filename); // call KSU hook first
 	if ((flag & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		      AT_EMPTY_PATH)) != 0)
 		goto out;
