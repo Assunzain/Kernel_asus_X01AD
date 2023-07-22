474b602269d5906328b314ec517fc00ef025ed3c
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-07-22 13:11:45 +0700

    X01AD: Patch KernelSU

--- fs/exec.c
+++ fs/exec.c
@@ -1682,7 +1682,6 @@ static int exec_binprm(struct linux_binprm *bprm)
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
 
-ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
