ec3c1aba7b8496cb65f0ff9d2b709d5348637044
AssunZain <Asepsunanda.zain@gmail.com>
2023-12-11 16:17:39 +0000

    Revert "tcp: Disable ping replies by default"
    This reverts commit 2c5f66822c51f988ca95fee1a2523fac2437cc20.

--- net/ipv4/icmp.c
+++ net/ipv4/icmp.c
@@ -1267,8 +1267,8 @@ static int __net_init icmp_sk_init(struct net *net)
 	}
 
 	/* Control parameters for ECHO replies. */
+	net->ipv4.sysctl_icmp_echo_ignore_all = 0;
 	net->ipv4.sysctl_icmp_echo_ignore_broadcasts = 1;
-	net->ipv4.sysctl_icmp_echo_ignore_all = 1;
 
 	/* Control parameter - ignore bogus broadcast responses? */
 	net->ipv4.sysctl_icmp_ignore_bogus_error_responses = 1;
