ae338ae51bc57db7a5cc1273794f53542440103a
AssunZain <Asepsunanda.zain@gmail.com>
2023-12-28 15:41:23 +0000

    Revert "Trying fix"
    This reverts commit 073958063e65782ed8514c45f4d016d7cc5e65bf.

--- drivers/thermal/msm_thermal_simple.c
+++ drivers/thermal/msm_thermal_simple.c
@@ -48,7 +48,7 @@ static void update_online_cpu_policy(void)
 	get_online_cpus();
 	cpu = cpumask_first_and(cpu_lp_mask, cpu_online_mask);
 	cpufreq_update_policy(cpu);
-	cpu = cpumask_first_and(cpu_cpu_mask, cpu_online_mask);
+	cpu = cpumask_first_and(cpu_perf_mask, cpu_online_mask);
 	cpufreq_update_policy(cpu);
 	put_online_cpus();
 }
@@ -91,7 +91,7 @@ reschedule:
 
 static u32 get_throttle_freq(struct thermal_zone *zone, u32 cpu)
 {
-	if (cpumask_test_cpu(cpu, cpu_cpu_mask))
+	if (cpumask_test_cpu(cpu, cpu_lp_mask))
 		return zone->silver_khz;
 
 	return zone->gold_khz;
