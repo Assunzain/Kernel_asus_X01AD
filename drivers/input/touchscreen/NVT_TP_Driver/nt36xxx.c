508f43065e6d604187b30978e518828abe101ec5
Rocky7842 <eric.rocky7842@gmail.com>
2024-01-08 17:16:15 +0800

    drivers: touchscreen: NVT: Correct wakeup_source typo

--- drivers/input/touchscreen/NVT_TP_Driver/nt36xxx.c
+++ drivers/input/touchscreen/NVT_TP_Driver/nt36xxx.c
@@ -868,7 +868,7 @@ int nvt_test_node_init(struct platform_device *tpinfo_device)
 /* function page definition */
 #define FUNCPAGE_GESTURE         1
 
-struct wakeup_source gestrue_wakelock;
+struct wakeup_source gesture_wakelock;
 
 /*******************************************************
 Description:
@@ -1248,7 +1248,7 @@ static irqreturn_t nvt_ts_irq_handler(int32_t irq, void *dev_id)
 
 #if WAKEUP_GESTURE
 	if (bTouchIsAwake == 0) {
-		__pm_wakeup_event(&gestrue_wakelock, 5000);
+		__pm_wakeup_event(&gesture_wakelock, 5000);
 	}
 #endif
 
@@ -1555,7 +1555,7 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 	for (retry = 0; retry < (sizeof(gesture_key_array) / sizeof(gesture_key_array[0])); retry++) {
 		input_set_capability(ts->input_dev, EV_KEY, gesture_key_array[retry]);
 	}
-	wakeup_source_init(&gestrue_wakelock, "poll-wake-lock");
+	wakeup_source_init(&gesture_wakelock, "poll-wake-lock");
 #endif
 
 	sprintf(ts->phys, "input/ts");
