a0b46482085859c1d2ea01d639dad6748c9e285e
AssunZain <Asepsunanda.zain@gmail.com>
2025-02-27 14:23:26 +0000

    ksu: addapted SuSFS

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -2,6 +2,23 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
+#endif
+
+ static int cmdline_proc_show(struct seq_file *m, void *v)
+ {
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+	if (!susfs_spoof_cmdline_or_bootconfig(m)) {
+		seq_putc(m, '\n');
+		return 0;
+	}
+#endif
+ 	seq_printf(m, "%s\n", saved_command_line);
+ 	return 0;
+ }
+
 #include <asm/setup.h>
 
 static char new_command_line[COMMAND_LINE_SIZE];
