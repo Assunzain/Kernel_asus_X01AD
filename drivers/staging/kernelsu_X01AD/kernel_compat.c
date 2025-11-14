fc8658f3d4da3c7d1708eca1387b05af57f1a2c4
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2025-11-14 17:05:35 +0700

    ksu: sync KSU NEXT v1.1.1 + SUSFS v1.5.7 (2/1)

--- drivers/staging/kernelsu_X01AD/kernel_compat.c
+++ drivers/staging/kernelsu_X01AD/kernel_compat.c
@@ -183,3 +183,17 @@ long ksu_strncpy_from_user_nofault(char *dst, const void __user *unsafe_addr,
 	return ret;
 }
 #endif
+
+long ksu_strncpy_from_user_retry(char *dst, const void __user *unsafe_addr,
+				   long count)
+{
+	long ret = ksu_strncpy_from_user_nofault(dst, unsafe_addr, count);
+	if (likely(ret >= 0))
+		return ret;
+
+	// we faulted! fallback to slow path
+	if (unlikely(!ksu_access_ok(unsafe_addr, count)))
+		return -EFAULT;
+
+	return strncpy_from_user(dst, unsafe_addr, count);
+}
